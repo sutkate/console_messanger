d6c64da562bd10d17ba5bd45496346a72730e882
sutkate <arseniykhokhlov123@gmail.com>
2024-07-14 17:12:48 +0300

    added some files

--- ConsoleApplication3/client.hpp
+++ ConsoleApplication3/client.hpp
@@ -1,11 +1,15 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include "user.hpp"
+#include "server.hpp"
 class Client
 {
 private:
 	User* current;
 public:
 	Client();
-	void send_message(string message, string username);
+	void send_message(string message, string username, User* current);
+	void check_message(User* current);
+	
 };
--- ConsoleApplication3/server.hpp
+++ ConsoleApplication3/server.hpp
@@ -1,2 +1,12 @@
 #include <iostream>
 #include "user.hpp"
+#include "vector"
+
+class Server
+{
+private:
+	std::vector <User> users;
+	std::vector 
+public:
+	void new_user(User user) { users.push_back(user); }
+};
